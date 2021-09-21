ebbaeef2bb8e97c560a5e4f4fdf5bc10da8243c1
priscilla <pdjarbeng@outlook.com>
2021-09-21 13:07:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+// Start writing code here
+let num1=31
+let num2=2
+let multiply = num1 * num2
+
+let random=3
+Math.floor(Math.random() * 10);
+
+let num3=10
+let num4=6
+let mod = num3 % num4
+
+Math.max(1, 2, 3, 4, 20)
+let max=20
